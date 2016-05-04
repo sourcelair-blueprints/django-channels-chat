d74942bcf1b76c5d301ec93202d58fd10f21142f
Antonis Kalipetis <akalipetis@gmail.com>
2016-05-04 18:40:26 +0300

    Add rooms in group chats

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,11 +1,16 @@
 from channels import Group
+from channels.sessions import channel_session
 
 
-def ws_add(message):
-    Group('chat').add(message.reply_channel)
+@channel_session
+def ws_add(message, room):
+    Group('chat-%s' % room).add(message.reply_channel)
+    message.channel_session['room'] = room
 
 
+@channel_session
 def ws_echo(message):
-    Group('chat').send({
+    room = message.channel_session['room']
+    Group('chat-%s' % room).send({
         'text': message.content['text'],
     })
--- django_channels/routing.py
+++ django_channels/routing.py
@@ -3,5 +3,6 @@ from channels.routing import route
 
 channel_routing = [
     route('websocket.receive', 'chat.consumers.ws_echo'),
-    route('websocket.connect', 'chat.consumers.ws_add'),
+    route('websocket.connect', 'chat.consumers.ws_add',
+          path=r'^/chat/(?P<room>\w+)$'),
 ]
