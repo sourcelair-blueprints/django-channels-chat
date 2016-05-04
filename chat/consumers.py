c45dcf230e8a8b4f9743cb709d74ac7f7ca4962a
Antonis Kalipetis <akalipetis@gmail.com>
2016-05-04 18:09:08 +0300

    Add group messaging

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,4 +1,11 @@
+from channels import Group
+
+
+def ws_add(message):
+    Group('chat').add(message.reply_channel)
+
+
 def ws_echo(message):
-    message.reply_channel.send({
+    Group('chat').send({
         'text': message.content['text'],
     })
--- django_channels/routing.py
+++ django_channels/routing.py
@@ -3,4 +3,5 @@ from channels.routing import route
 
 channel_routing = [
     route('websocket.receive', 'chat.consumers.ws_echo'),
+    route('websocket.connect', 'chat.consumers.ws_add'),
 ]
