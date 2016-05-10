90d5b592dd1e2fb42cfcc37d39a18a4930ca6fe0
Antonis Kalipetis <akalipetis@gmail.com>
2016-05-10 17:18:21 +0300

    Add username support for connections

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,16 +1,28 @@
+import json
+import urlparse
+
 from channels import Group
 from channels.sessions import channel_session
 
 
 @channel_session
 def ws_add(message, room):
+    query = urlparse.parse_qs(message['query_string'])
+    if 'username' not in query:
+        return
     Group('chat-%s' % room).add(message.reply_channel)
     message.channel_session['room'] = room
+    message.channel_session['username'] = query['username'][0]
 
 
 @channel_session
 def ws_echo(message):
+    if 'username' not in message.channel_session:
+        return
     room = message.channel_session['room']
     Group('chat-%s' % room).send({
-        'text': message.content['text'],
+        'text': json.dumps({
+            'message': message.content['text'],
+            'username': message.channel_session['username']
+        }),
     })
